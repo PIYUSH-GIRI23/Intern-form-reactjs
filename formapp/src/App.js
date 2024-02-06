ebeb28f528e57f4aad0fc76fe3e23decbacb206f
Piyush Giri <piyush.giri232003@gmail.com>
2024-02-06 09:41:28 +0530

    changed readme and added navbar

--- formapp/src/App.js
+++ formapp/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <BrowserRouter>
+          <Navbar />
+        </BrowserRouter>
       </div>
     );
   }
---
+++ formapp/src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/">Medicare</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link" aria-current="page" href="https://github.com/PIYUSH-GIRI23/Intern-form-reactjs" target="_blank">Frontend (Github)</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="https://github.com/PIYUSH-GIRI23/Intern-form">Backend (Github)</a>
+                            </li>
+                        </ul>
+                        <form className="d-flex" role="search">
+                            <button className="btn btn-outline-success" type="submit">Admin</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
